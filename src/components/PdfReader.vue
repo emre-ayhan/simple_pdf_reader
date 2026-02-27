29d42bfb7cfdb6081e29f7033cb3b28c06786094
emre-ayhan <mr.emreayhan@gmail.com>
2026-02-27 01:34:46 +0100

    modified:   src/components/PdfReader.vue

--- src/components/PdfReader.vue
+++ src/components/PdfReader.vue
@@ -17,6 +17,7 @@ import PdfForm from "./PdfForm.vue";
 
 // Cursor Style
 const cursorStyle = computed(() => {
+    const penCursorColor = encodeURIComponent(drawColor.value);
     if (resizeCursor.value) return resizeCursor.value;
     if (handToolActive.value) return isHandToolPanning.value ? 'grabbing' : 'grab';
     if (selectedStroke.value && (isStrokeHovering.value || isDragging.value)) return 'move';
@@ -24,7 +25,7 @@ const cursorStyle = computed(() => {
     if (isSelectionMode.value) return 'crosshair';
     if (isTextInputMode.value) return 'text';
     if (isDrawing.value ) {
-        if(drawMode.value == 'pen') return `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${drawColor.value}" class="bi bi-pencil-fill" viewBox="0 0 16 16"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/></svg>') 8 8, auto`;
+        if(drawMode.value === 'pen') return `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${penCursorColor}" class="bi bi-pencil-fill" viewBox="0 0 16 16"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/></svg>') 8 8, auto`;
         if(drawMode.value == 'highlight') return 'text';
         return 'crosshair';
     };
@@ -853,7 +854,7 @@ defineExpose({
                                     aria-expanded="false"
                                     :style="{ backgroundColor: selectedStroke?.stroke[0]?.color || 'transparent' }"
                                 ></button>
-                                <div class="dropdown-menu dropdown-menu-dark color-menu px-2">
+                                <div class="dropdown-menu dropdown-menu-dark color-menu px-2 mt-2">
                                     <button 
                                         v-for="strokeStyle in initialStrokeStyles"
                                         class="btn-color dropdown-item mb-1"
